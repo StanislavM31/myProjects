a91c4eabdf3314ef0c669514f09cc1baf21a07d1
Stanislav Minko <minkostas@gmail.com>
2022-02-18 01:54:36 +0300

    feat: move ball

--- game/script.js
+++ game/script.js
@@ -2,8 +2,26 @@ console.log("Game");
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath()
+/* оси х,у */
+let x = canvas.width/2;
+let y = canvas.height-30;
+
+/* шаг */
+let dx = 2;
+let dy = -2;
+
+draw();
+
+function draw(){
+    console.log("drawFoo");
+
+    ctx.clearRect(0,0,canvas.width, canvas.height); // чистим след от предыдущ.прорисовки
+    ctx.beginPath();
+    ctx.arc(x, y, 5, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+    x = x +dx;
+    y = y +dy;
+}
+setInterval(draw, 100);
