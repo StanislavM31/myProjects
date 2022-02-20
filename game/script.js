5bbc9166ef2a508e747b107cf7e5cb9997f21ed7
Stanislav Minko <minkostas@gmail.com>
2022-02-20 04:08:10 +0300

    feat: bounce

--- game/script.js
+++ game/script.js
@@ -1,30 +1,72 @@
-console.log("Game");
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var ballRadius = 10;
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+var rightPressed = false;
+var leftPressed = false;
 
-/* оси х,у */
-let x = canvas.width/2;
-let y = canvas.height-30;
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 
-/* шаг */
-let dx = 2;
-let dy = -2;
+function keyDownHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
 
-
-function drawBall(){
-    console.log("drawFoo");
-
+function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 5, 0, Math.PI*2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
+}
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
 
-}
-function clearTrack(){
-    ctx.clearRect(0,0,canvas.width, canvas.height); // чистим след от предыдущ.прорисовки
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
-    x = x +dx;
-    y = y +dy;
+    drawPaddle();
+    
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+    
+    if(rightPressed && paddleX < canvas.width-paddleWidth) {
+        paddleX += 7;
+    }
+    else if(leftPressed && paddleX > 0) {
+        paddleX -= 7;
+    }
+    
+    x += dx;
+    y += dy;
 }
-setInterval(clearTrack, 100);
+
+setInterval(draw, 10);
